6a188eb8e462c5fe0bf2e6a2ea9368a0118cefab
Gábor <yesszus@gmail.com>
2016-09-24 09:14:04 +0200

    Add files via upload

---
+++ languages/Hungarian_global.php
@@ -0,0 +1,50 @@
+<?php
+/*
++---------------------------------------------------------------+
+|        e107 website content management system Hungarian Language File
+|        Released under the terms and conditions of the
+|        GNU General Public License (http://gnu.org).
+|        Plugin file :  e107_plugins/userlanguage_flags_menu/languages/English.php
+|        Revision    1.5
+|        Date        26.07.2013
+|        Author      JmoRava, Oxigen ( www.e107.funsite.cz )
+|
+|        Last Modified: 2016/09/24 08:01:20
+|        $Author: Yesszus $
++---------------------------------------------------------------+
+*/
+
+//*MENU*//
+define("USLFM_A_L1", "A beállítások elmentve!");
+define("USLFM_A_L2", "Menü felirat");
+define("USLFM_A_L3", "Menü Beállítások Frissítése");
+define("USLFM_A_L4", "Opció Beállítások");
+define("USLFM_A_L5", "Menü címének mutatása vagy elrejtése:");
+define("USLFM_A_L6", "Jelölje be a menü címének megjelenítéséhez vagy elrejtéséhez.");
+define("USLFM_A_L7", "Zászló stilus kiválasztása:");
+define("USLFM_A_L8", "Zászló maximum szélesség:");
+define("USLFM_A_L9", "px (1-99px)");
+define("USLFM_A_L10", "Nyelv megjelenítése legördülő menüben:");
+define("USLFM_A_L11", "Jelölje be a nyelv szöveges megjelenítéséhez vagy elrejtéséhez.");
+define("USLFM_A_L12", "Adja meg a saját menü címet, ha szeretné, hogy az alapértelmezett név maradjon, akkor hagyja üresen.");
+define("USLFM_A_L13", "Zászló megjelenítése:");
+define("USLFM_A_L14", "Bal oldalon");
+define("USLFM_A_L15", "Középen");
+define("USLFM_A_L16", "Jobb oldalon");
+//* PLUGIN *//
+define("USLFM_P_1", "Oldal Nyelv");
+define("USLFM_P_2", "A menü lehetővé teszi a felhasználóknak az oldal nyelvének kiválasztását.");
+define("USLFM_P_3", "Nyelv kiválasztása");
+define("USLFM_P_4", "Menü beállítás");
+define("USLFM_P_5", "Kiválasztás");
+define("USLFM_P_6", "Telepítés");
+define("USLFM_P_7", "A frissítés erre: ");
+define("USLFM_P_8", "sikeresen lezajlott!");
+//* HELP *//
+define("USLFM_H_1", "<b>Sugó</b>");
+define("USLFM_H_3", "<b>View menu or flags:</b><br/> This option allows you to display the graphics options (flags), or the default drop-down menu.");
+define("USLFM_H_4", "<b>Entering size flags:</b><br/> Flags are about the size of 100px. This option lets you choose the range of x-display size of 99px.");
+define("USLFM_H_5", "<b>View menu or flags:</b> <br/> This option allows you to display the graphics options (flags), or the default drop-down menu.");
+define("USLFM_H_6", "<b>Alignment flags:</b><br/> Since version 1.4 it is possible to Flag positioned in the menu.");
+define("USLFM_H_7", "<b>Integration pages:</b><br/> Menu can be used to directly integrate  using the code {ULFLAGS}.");
+?>
